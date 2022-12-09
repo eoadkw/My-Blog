4caaf9c496129ded30ab70ad93462e2621b74ea3
eoadkw <als020213@naver.com>
2022-12-09 16:02:23 +0900

    1

---
+++ data/metadata.ts
@@ -0,0 +1,8 @@
+const metadata = {
+  title: 'Attraction',
+  description: "who's blog",
+  author: 'Yeoungmin Eo',
+  name: 'Yeoung min',
+};
+
+export default metadata;
---
+++ data/navlinks.ts
@@ -0,0 +1,6 @@
+const navlinks: { title: string; link: string }[] = [
+  { title: 'HOME', link: '/' },
+  { title: 'POST', link: '/blog' },
+];
+
+export default navlinks;
